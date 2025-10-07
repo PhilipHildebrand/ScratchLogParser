0d0d06dc2327c82c632e5532e1b95a0ece6a88d3
PhilipHildebrand <99822700+PhilipHildebrand@users.noreply.github.com>
2025-10-07 22:28:31 +0200

    Add files via upload
    Push parser

---
+++ main/java/org/example/WriteEntry.java
@@ -0,0 +1,204 @@
+package org.example;
+
+public class WriteEntry {
+
+    public ActionTypes actionType;
+
+    public String group;
+
+    public String begin;
+
+    public String end;
+
+    public String blockID;
+
+    public String oldValue;
+
+    public String newValue;
+
+    public String category;
+
+    public String blockCount;
+
+    public String conditionalInvolved;
+
+    public String loopInvolved;
+
+    public String oldParentId;
+
+    public String newParentId;
+
+    public String valueType;
+
+    public String scriptId;
+
+    public String scriptIdAfterMove;
+
+    public String spritename;
+
+    public String metadata;
+
+    public String oldInputName;
+
+    public String newInputName;
+
+    public ActionTypes getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(ActionTypes actionType) {
+        this.actionType = actionType;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getBegin() {
+        return begin;
+    }
+
+    public void setBegin(String begin) {
+        this.begin = begin;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getBlockID() {
+        return blockID;
+    }
+
+    public void setBlockID(String blockID) {
+        this.blockID = blockID;
+    }
+
+    public String getOldValue() {
+        return oldValue;
+    }
+
+    public void setOldValue(String oldValue) {
+        this.oldValue = oldValue;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getNewValue() {
+        return newValue;
+    }
+
+    public void setNewValue(String newValue) {
+        this.newValue = newValue;
+    }
+
+    public String getBlockCount() {
+        return blockCount;
+    }
+
+    public void setBlockCount(String blockCount) {
+        this.blockCount = blockCount;
+    }
+
+    public String getConditionalInvolved() {
+        return conditionalInvolved;
+    }
+
+    public void setConditionalInvolved(String conditionalInvolved) {
+        this.conditionalInvolved = conditionalInvolved;
+    }
+
+    public String getLoopInvolved() {
+        return loopInvolved;
+    }
+
+    public void setLoopInvolved(String loopInvolved) {
+        this.loopInvolved = loopInvolved;
+    }
+
+    public String getOldParentId() {
+        return oldParentId;
+    }
+
+    public void setOldParentId(String oldParentId) {
+        this.oldParentId = oldParentId;
+    }
+
+    public String getNewParentId() {
+        return newParentId;
+    }
+
+    public void setNewParentId(String newParentId) {
+        this.newParentId = newParentId;
+    }
+
+    public String getScriptId() {
+        return scriptId;
+    }
+
+    public void setScriptId(String scriptId) {
+        this.scriptId = scriptId;
+    }
+
+    public String getScriptIdAfterMove() {
+        return scriptIdAfterMove;
+    }
+
+    public void setScriptIdAfterMove(String scriptIdAfterMove) {
+        this.scriptIdAfterMove = scriptIdAfterMove;
+    }
+
+    public String getSpritename() {
+        return spritename;
+    }
+
+    public void setSpritename(String spritename) {
+        this.spritename = spritename;
+    }
+
+    public String getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(String metadata) {
+        this.metadata = metadata;
+    }
+
+    public String getOldInputName() {
+        return oldInputName;
+    }
+
+    public void setOldInputName(String oldInputName) {
+        this.oldInputName = oldInputName;
+    }
+
+    public String getNewInputName() {
+        return newInputName;
+    }
+
+    public void setNewInputName(String newInputName) {
+        this.newInputName = newInputName;
+    }
+
+    public String getValueType() {
+        return valueType;
+    }
+
+    public void setValueType(String valueType) {
+        this.valueType = valueType;
+    }
+}
